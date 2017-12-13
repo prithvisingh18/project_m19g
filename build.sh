c6ed1aa7fb7c5f485837a9dafb4b124da3bff268
Prithvi Singh <prithvi.chandla@gmail.com>
2017-12-13 10:19:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
 #em++ -s WASM=1 multiply.cpp -o multiply.js
-emcc --bind -s WASM=1 -o multiply.js multiply.cpp #--js-library multiply_in.js
+emcc --bind -s WASM=1 -o multiply.js multiply.cpp --js-library multiply_in.js
