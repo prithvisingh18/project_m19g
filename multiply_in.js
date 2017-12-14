bd19d96c0cc15d22cb524fae74f2f784f16f6ca7
Prithvi Singh <prithvi.chandla@gmail.com>
2017-12-14 21:36:55 +0530

    Delete multiply_in.js

--- multiply_in.js
+++
@@ -1,6 +0,0 @@
-mergeInto(LibraryManager.library, {
-  multiply_in_js: function(x,y) {
-    document.getElementById("output_area").append("multiplying in js : " + x + " * " + y+ "\n\n");
-    return x * y;
-  },
-});
